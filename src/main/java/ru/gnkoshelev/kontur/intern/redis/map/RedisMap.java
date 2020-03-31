0ca76f7ab96db168434f12f75fd2e28f8e21e2ac
Arkron <ArkronCherf01@gmail.com>
2020-03-31 20:13:21 +0300

    Add missing files

--- src/main/java/ru/gnkoshelev/kontur/intern/redis/map/RedisMap.java
+++ src/main/java/ru/gnkoshelev/kontur/intern/redis/map/RedisMap.java
@@ -1,70 +1,216 @@
 package ru.gnkoshelev.kontur.intern.redis.map;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import redis.clients.jedis.Jedis;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * @author Gregory Koshelev
  */
-public class RedisMap implements Map<String,String> {
+public class RedisMap implements Map<String, String> {
+
+    private static final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private static final Jedis jedis = new Jedis();
+    private String hashUUID;
+
+    private String lockTable;
+
+    public RedisMap() {
+        this(UUID.randomUUID().toString());
+    }
+
+    public RedisMap(String hashUUID) {
+        checkNullObject(hashUUID);
+        this.hashUUID = hashUUID;
+        lockTable = "lock#" + hashUUID;
+        lock.writeLock().lock();
+        try {
+            jedis.lpush(lockTable, "ok");
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    public String getHashUUID() {
+        return hashUUID;
+    }
+
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        lock.readLock().lock();
+        try {
+            return jedis.hgetAll(hashUUID).size();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return size() == 0;
     }
 
     @Override
     public boolean containsKey(Object key) {
-        throw new UnsupportedOperationException();
+        checkNullObject(key);
+        lock.readLock().lock();
+        try {
+            return jedis.hget(hashUUID, (String) key) != null;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public boolean containsValue(Object value) {
-        throw new UnsupportedOperationException();
+        checkNullObject(value);
+        lock.readLock().lock();
+        try {
+            return jedis.hgetAll(hashUUID).containsValue(value);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public String get(Object key) {
-        throw new UnsupportedOperationException();
+        checkNullObject(key);
+        lock.readLock().lock();
+        try {
+            return jedis.hget(hashUUID, (String) key);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public String put(String key, String value) {
-        throw new UnsupportedOperationException();
+        checkNullObject(key);
+        checkNullObject(value);
+        lock.writeLock().lock();
+        try {
+            String val = jedis.hget(hashUUID, key);
+            jedis.hset(hashUUID, key, value);
+            return val;
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     @Override
     public String remove(Object key) {
-        throw new UnsupportedOperationException();
+        checkNullObject(key);
+        lock.writeLock().lock();
+        try {
+            String val = jedis.hget(hashUUID, (String) key);
+            if (val != null) {
+                jedis.hdel(hashUUID, (String) key);
+            }
+            return val;
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     @Override
     public void putAll(Map<? extends String, ? extends String> m) {
-        throw new UnsupportedOperationException();
+        Map<String, String> data = new HashMap<>();
+        m.forEach((k, v) -> {
+            checkNullObject(k);
+            checkNullObject(v);
+            data.put(k, v);
+        });
+        lock.writeLock().lock();
+        try {
+            jedis.hset(hashUUID, data);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException();
+        lock.writeLock().lock();
+        try {
+            jedis.hkeys(hashUUID).forEach(key ->
+                    jedis.hdel(hashUUID, key));
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     @Override
     public Set<String> keySet() {
-        throw new UnsupportedOperationException();
+        lock.readLock().lock();
+        try {
+            return jedis.hkeys(hashUUID);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public Collection<String> values() {
-        throw new UnsupportedOperationException();
+        lock.readLock().lock();
+        try {
+            return jedis.hvals(hashUUID);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public Set<Entry<String, String>> entrySet() {
-        throw new UnsupportedOperationException();
+        lock.readLock().lock();
+        try {
+            return jedis.hgetAll(hashUUID).entrySet();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        close();
+    }
+
+    private void checkNullObject(Object obj) {
+        if (obj == null) {
+            throw new IllegalArgumentException("Argument can't be null");
+        }
+    }
+
+    private void close() {
+        lock.writeLock().lock();
+        try {
+            jedis.lpop(lockTable);
+        } finally {
+            lock.writeLock().unlock();
+        }
+        AtomicLong len = new AtomicLong();
+        lock.readLock().lock();
+        try {
+            len.compareAndSet(0, jedis.llen(lockTable));
+        } finally {
+            lock.readLock().unlock();
+        }
+        if (len.get() == 0) {
+            clear();
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj instanceof RedisMap) {
+            return this.hashUUID.equals(((RedisMap) obj).hashUUID) || this.entrySet().equals(((RedisMap) obj).entrySet());
+        }
+        return false;
     }
 }
--- src/test/java/ru/gnkoshelev/kontur/intern/redis/map/RedisMapTest.java
+++ src/test/java/ru/gnkoshelev/kontur/intern/redis/map/RedisMapTest.java
@@ -3,9 +3,12 @@ package ru.gnkoshelev.kontur.intern.redis.map;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.lang.Thread.sleep;
 
 /**
  * @author Gregory Koshelev
@@ -41,5 +44,141 @@ public class RedisMapTest {
         Collection<String> values1 = map1.values();
         Assert.assertEquals(1, values1.size());
         Assert.assertTrue(values1.contains("first"));
+
+        map1.clear();
+        Map<String, String> res = Map.of("1", "a", "2", "b");
+        map1.putAll(res);
+        Assert.assertEquals(map1.get("1"), "a");
+        Assert.assertEquals(map1.get("2"), "b");
+    }
+
+    @Test
+    public void baseTest2() {
+        RedisMap redisMap = new RedisMap();
+        Map<String, String> redisMap2 = new RedisMap();
+        Map<String, String> redisMap3 = new RedisMap(redisMap.getHashUUID());
+        Map<String, String> hashMap = new HashMap<>();
+
+        redisMap.put("1", "2");
+        redisMap2.put("1", "2");
+
+        Assert.assertEquals(redisMap, redisMap);
+
+        Assert.assertEquals(redisMap, redisMap2);
+        Assert.assertEquals(redisMap2, redisMap);
+
+        Assert.assertEquals(redisMap, redisMap2);
+        Assert.assertEquals(redisMap2, redisMap3);
+        Assert.assertEquals(redisMap3, redisMap);
+
+        redisMap.remove("1");
+        Assert.assertNotEquals(redisMap, redisMap2);
+
+        hashMap.put("a", "a");
+        hashMap.put("b", "b");
+        hashMap.put("c", "c");
+        hashMap.put("d", "d");
+        redisMap.putAll(hashMap);
+        Set<Map.Entry<String, String>> entries = redisMap.entrySet();
+        entries.forEach(e -> Assert.assertEquals(hashMap.get(e.getKey()), e.getValue()));
+
+        Assert.assertEquals("a", redisMap.remove("a"));
+        Assert.assertNull(redisMap.remove("5"));
+
+        Assert.assertNull(redisMap.put("a", "a"));
+        Assert.assertEquals("a", redisMap.put("a", "b"));
+        Assert.assertFalse(redisMap.isEmpty());
+
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exceptionTest() {
+        Map<String, String> map1 = new RedisMap();
+        map1.put(null, "12");
+    }
+
+    @Test
+    public void cleanTest() throws InterruptedException {
+        String uuid = UUID.randomUUID().toString();
+        RedisMap map = new RedisMap(uuid);
+        Assert.assertEquals(uuid, map.getHashUUID());
+        map.put("1", "2");
+        map = null;
+        System.gc();
+        sleep(100L);
+        RedisMap testClean = new RedisMap(uuid);
+        Assert.assertEquals(0, testClean.size());
+
+    }
+
+    @Test
+    public void concurrentTest() throws ExecutionException, InterruptedException {
+        String uuid = UUID.randomUUID().toString();
+        RedisMap map = new RedisMap(uuid);
+        RedisMap map2 = new RedisMap(uuid);
+        map.put("1", "2");
+        Assert.assertEquals("2", map2.get("1"));
+        Assert.assertEquals(map, map2);
+
+        map2.clear();
+        Assert.assertTrue(map.isEmpty());
+        Assert.assertTrue(map2.isEmpty());
+        Assert.assertEquals(0, map.size());
+
+        CompletableFuture.allOf(
+                CompletableFuture.runAsync(() -> map.putAll(Map.of("3", "4"))),
+                CompletableFuture.runAsync(() -> map2.putAll(Map.of("4", "5"))),
+                CompletableFuture.runAsync(() -> map.putAll(Map.of("1", "2"))),
+                CompletableFuture.runAsync(() -> map2.putAll(Map.of("3", "4")))
+        ).get();
+        Assert.assertEquals("5", map.get("4"));
+
+        CompletableFuture.allOf(
+                CompletableFuture.runAsync(() -> map.put("1", "2")),
+                CompletableFuture.runAsync(() -> map2.remove("1"))
+        ).get();
+        Assert.assertNull(map.get("1"));
+
+        MultithreadedStressTester stressTester = new MultithreadedStressTester(6, 25000);
+        map.clear();
+        AtomicInteger i = new AtomicInteger();
+        stressTester.stress(() -> {
+            i.addAndGet(1);
+            map.put(String.valueOf(i.get()), "test");
+        });
+
+        stressTester.shutdown();
+        Assert.assertEquals(150000, map.size());
+
+        stressTester = new MultithreadedStressTester(6, 25000);
+        AtomicInteger j = new AtomicInteger();
+        stressTester.stress(() -> {
+            j.addAndGet(1);
+            map.remove(String.valueOf(j.get()));
+        });
+        stressTester.shutdown();
+        Assert.assertEquals(0, map.size());
+    }
+
+    @Test
+    public void finalizeTest() throws InterruptedException {
+        String uuid = UUID.randomUUID().toString();
+        RedisMap map = new RedisMap(uuid);
+        map.put("1", "2");
+        RedisMap map3 = new RedisMap();
+        RedisMap testClean = new RedisMap(uuid);
+        map = null;
+        System.gc();
+        sleep(100L);
+        Assert.assertEquals(1, testClean.size());
+        Assert.assertEquals(0, map3.size());
+
+        testClean = null;
+        System.gc();
+        sleep(100L);
+
+        RedisMap map2 = new RedisMap(uuid);
+        Assert.assertEquals(0, map2.size());
     }
 }
